5f7ab1fc477ed90689f96b753ef56e5dbfefead7
astro2coldd <158317608+astro2coldd@users.noreply.github.com>
2024-01-31 17:13:09 +0000

    Update Keep_alive.js

--- Keep_alive.js
+++ Keep_alive.js
@@ -1,1 +1,9 @@
-
+var http = require('http');
+function keepAlive() {
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
+}
+module.exports = keepAlive;
+//End of code
